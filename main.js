a7bc22f6befd2a20ed03d7e65b55427562df0730
Ronald Rojas Newball <rrnewball@gmail.com>
2022-09-16 15:12:01 -0500

    type of functions

---
+++ main.js
@@ -0,0 +1,27 @@
+/*DECLARADA*/
+
+
+
+// function sumar(a,b){
+//     let suma=a+b
+//     console.log(suma)
+// }
+
+// sumar(2,1)
+/*EXPRESADA */
+
+// const sumar =  function(a,b){
+//     let suma=a+b
+//     console.log(suma)
+// }
+
+// sumar(2,1)
+
+/*ARROW FUNCTION*/
+// const sumar = (a,b)=>{
+//     let suma=a+b
+//     console.log(suma)
+// }
+// sumar(2,3)
+
+let number=prompt("Give me a number")
